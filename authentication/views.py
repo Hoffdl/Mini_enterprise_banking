30aaf0a9f4d25d10b07fc94dfb22738fd1d9155c
Henry <ikegwuonuhenry25@gmail.com>
2024-01-08 20:47:16 +0100

    Activation

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import 
+from django.contrib.auth import authenticate, login, logout
+from djangologin import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from . tokens import generate_token
+from django.core.mail import EmailMessage, send_mail
 
 # Create your views here.
 def home(request):
@@ -19,13 +27,60 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
         
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists! Please try another username")
+            return redirect('home')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+        
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 characters")
+             
+        if pass1 != pass2:
+            messages.error(request, "Passwords don't match")
+            
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!")
+            return redirect('home')
+        
+        
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        
+        myuser.is_active = False
         myuser.save()
         
         messages.success(request, "Your account has been successfully created.")
+        
+        
+        # Welcome Email
+        
+        subject = "Welcome to Mydemo app - Django Login!!"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to Mydemo app!! \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm your email address in order to activate your account. \n\n Thanking You\n Henry"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+        
+        # Email Address confirmation
+        
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @ djangologin - Django Login!!"
+        message2 = render_to_string('email_confirmation.html'){
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        }
+        email = EmailMessage(
+            email_subject,
+            message2,
+            setting.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
         
         return redirect('signin')
     
@@ -53,4 +108,21 @@ def signin(request):
     return render(request, "authentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_ decode(uid64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+    
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
