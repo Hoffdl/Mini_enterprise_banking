ce07ed6399cffb57780c29685b571b37d060b026
Henry <ikegwuonuhenry25@gmail.com>
2024-01-09 00:17:02 +0100

    authentication

--- authentication/views.py
+++ authentication/views.py
@@ -3,20 +3,13 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from djangologin import settings
-from django.core.mail import send_mail
-from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_text
-from . tokens import generate_token
-from django.core.mail import EmailMessage, send_mail
 
 # Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
+    
     
     if request.method == "POST":
         # username = request.POST.get('username')
@@ -29,10 +22,6 @@ def signup(request):
         
         if User.objects.filter(username=username):
             messages.error(request, "Username already exists! Please try another username")
-            return redirect('home')
-        
-        if User.objects.filter(email=email):
-            messages.error(request, "Email already registered!")
             return redirect('home')
         
         if len(username)>10:
@@ -49,41 +38,10 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        myuser.is_active = False
         myuser.save()
         
-        messages.success(request, "Your account has been successfully created.")
+        messages.success(request, "Your account has been created successfully.")
         
-        
-        # Welcome Email
-        
-        subject = "Welcome to Mydemo app - Django Login!!"
-        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to Mydemo app!! \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm your email address in order to activate your account. \n\n Thanking You\n Henry"
-        from_email = settings.EMAIL_HOST_USER
-        to_list = [myuser.email]
-        send_mail(subject, message, from_email, to_list, fail_silently=True)
-        
-        # Email Address confirmation
-        
-        current_site = get_current_site(request)
-        email_subject = "Confirm your email @ djangologin - Django Login!!"
-        message2 = render_to_string('email_confirmation.html'){
-            'name': myuser.first_name,
-            'domain': current_site.domain,
-            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-            'token': generate_token.make_token(myuser)
-        }
-        email = EmailMessage(
-            email_subject,
-            message2,
-            setting.EMAIL_HOST_USER,
-            [myuser.email],
-        )
-        email.fail_silently = True
-        email.send()
-        
-        return redirect('signin')
-    
     
     return render(request, "authentication/signup.html")
 
@@ -100,7 +58,7 @@ def signin(request):
             login(request, user)
             fname = user.first_name
             return render(request, "authentication/index.html", {'fname': fname})
-            
+        
         else:
             messages.error(request, "Bad Credentials")
             return redirect('home')
@@ -111,18 +69,3 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged Out Successfully!!")
     return redirect('home')
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_ decode(uid64))
-        myuser = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        myuser = None
-    
-    if myuser is not None and generate_token.check_token(myuser, token):
-        myuser.is_active = True
-        myuser.save()
-        login(request, myuser)
-        return redirect('home')
-    else:
-        return render(request, 'activation_failed.html')
